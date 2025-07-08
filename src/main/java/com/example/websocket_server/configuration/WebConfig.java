44c49b3d70edac7aa7c133c386870c1e569e943a
이민성_N01 <lms8756@naver.com>
2025-07-08 15:32:41 +0900

    websocket + stomp 를 이용한 채팅서버

---
+++ src/main/java/com/example/websocket_server/configuration/WebConfig.java
@@ -0,0 +1,17 @@
+package com.example.websocket_server.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://192.168.106.80:5173") // 허용할 출처
+                .allowedMethods("GET", "POST") // 허용할 HTTP method
+                .allowCredentials(true) // 쿠키 인증 요청 허용
+                .maxAge(3000); // 원하는 시간만큼 pre-flight 리퀘스트를 캐싱
+    }
+}
