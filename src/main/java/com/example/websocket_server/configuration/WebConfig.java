f68478e644ab6edf95bf7cc810ed7eabd27bc07c
mystelee <lms8756@naver.com>
2025-07-13 13:03:40 +0900

    환경변수 추가

--- src/main/java/com/example/websocket_server/configuration/WebConfig.java
+++ src/main/java/com/example/websocket_server/configuration/WebConfig.java
@@ -1,5 +1,6 @@
 package com.example.websocket_server.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,9 +8,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${clientIp}")
+    private String clientIp;
+
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://192.168.106.80:5173") // 허용할 출처
+                .allowedOrigins(clientIp) // 허용할 출처
                 .allowedMethods("GET", "POST") // 허용할 HTTP method
                 .allowCredentials(true) // 쿠키 인증 요청 허용
                 .maxAge(3000); // 원하는 시간만큼 pre-flight 리퀘스트를 캐싱
